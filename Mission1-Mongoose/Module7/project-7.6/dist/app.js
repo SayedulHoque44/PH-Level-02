ea4308ba7980e80b1ebcc099eff3ef626f7658ef
sayedulhoque44 <sayedulhoque3544@gmail.com>
2023-11-16 14:06:28 +0600

    Error handling

--- Mission1-Mongoose/Module7/project-7.6/dist/app.js
+++ Mission1-Mongoose/Module7/project-7.6/dist/app.js
@@ -1,15 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express') --> amra ESM module system use krteci
+const console_1 = require("console");
 const express_1 = __importDefault(require("express")); //import syntax use krteci
 const app = (0, express_1.default)();
 // parser
 app.use(express_1.default.json()); // body te json data accept krte parbe
 app.use(express_1.default.text()); // body te plain text data accept krte parbe
-// Router
+//-------------> Router
 const userRouter = express_1.default.Router();
 // use router for users
 app.use("/api/v1/users", userRouter);
@@ -31,13 +41,32 @@ userRouter.get("/get-user", (req, res) => {
         },
     });
 });
-// middleware
+//--------> ulta palta url dile tkn segula catch korar jonno
+app.all("*", (req, res) => {
+    res.status(400).json({
+        status: "Error",
+        message: "Somthing Went Wrong!",
+    });
+});
+// ---------->Error handdling
+app.get("/error", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        res.send(somthing);
+    }
+    catch (err) {
+        next(console_1.error); // error poperty dewar karone ata onno kono handler kacehe na giye golbal error middleware er kache jabe
+        // res.status(404).send({
+        //   status: "Error",
+        //   message: "Faild to get Data",
+        // });
+    }
+}));
+// --------->middleware
 const checkvalidName = (req, res, next) => {
     if (req.query.name === "sayed") {
         next();
     }
-    res.statusCode = 403;
-    res.send({ message: "unable to access" });
+    res.status(403).send({ message: "unable to access" });
 };
 //req res
 app.get("/", (req, res) => {
@@ -55,5 +84,12 @@ app.get("/:userId", (req, res) => {
 app.post("/", (req, res) => {
     console.log(req.body);
     res.send(req.body);
+});
+// global error handler
+app.use((err, req, res, next) => {
+    res.status(400).send({
+        status: "Error",
+        message: "Somthing Went Wrong!",
+    });
 });
 exports.default = app;
--- Mission1-Mongoose/Module7/project-7.6/src/app/app.ts
+++ Mission1-Mongoose/Module7/project-7.6/src/app/app.ts
@@ -1,11 +1,12 @@
 // const express = require('express') --> amra ESM module system use krteci
+import { error } from "console";
 import express, { NextFunction, Request, Response } from "express"; //import syntax use krteci
 const app = express();
 // parser
 app.use(express.json()); // body te json data accept krte parbe
 app.use(express.text()); // body te plain text data accept krte parbe
 
-// Router
+//-------------> Router
 const userRouter = express.Router();
 
 // use router for users
@@ -30,13 +31,33 @@ userRouter.get("/get-user", (req: Request, res: Response) => {
   });
 });
 
-// middleware
+//--------> ulta palta url dile tkn segula catch korar jonno
+app.all("*", (req: Request, res: Response) => {
+  res.status(400).json({
+    status: "Error",
+    message: "Somthing Went Wrong!",
+  });
+});
+
+// ---------->Error handdling
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.send(somthing);
+  } catch (err) {
+    next(error); // error poperty dewar karone ata onno kono handler kacehe na giye golbal error middleware er kache jabe
+    // res.status(404).send({
+    //   status: "Error",
+    //   message: "Faild to get Data",
+    // });
+  }
+});
+
+// --------->middleware
 const checkvalidName = (req: Request, res: Response, next: NextFunction) => {
   if (req.query.name === "sayed") {
     next();
   }
-  res.statusCode = 403;
-  res.send({ message: "unable to access" });
+  res.status(403).send({ message: "unable to access" });
 };
 
 //req res
@@ -55,6 +76,14 @@ app.get("/:userId", (req: Request, res: Response) => {
 app.post("/", (req: Request, res: Response) => {
   console.log(req.body);
   res.send(req.body);
+});
+
+// global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).send({
+    status: "Error",
+    message: "Somthing Went Wrong!",
+  });
 });
 
 export default app;
